1f93c38b0d578f50dae9768e8720c0a40c475fe5
joeg-ita <joeg.ita@gmail.com>
2025-04-29 15:16:01 +0200

    Create config.go

---
+++ src/external/config/config.go
@@ -0,0 +1,166 @@
+package config
+
+import (
+	"errors"
+	"log"
+	"os"
+	"reflect"
+
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	Development        = "dev"
+	Production         = "prod"
+	ConfigBaseFileName = "/config"
+)
+
+type Database struct {
+	Url      string `yaml:"url" env:"GIOBBA_DATABASE_URL"`
+	Port     string `yaml:"port" env:"GIOBBA_DATABASE_PORT"`
+	Username string `yaml:"username" env:"GIOBBA_DATABASE_ADMIN_USERNAME"`
+	Password string `yaml:"password" env:"GIOBBA_DATABASE_ADMIN_PASSWORD"`
+  DB string `yaml:"db" env:"GIOBBA_DATABASE_ADMIN_PASSWORD"`
+}
+
+type Broker struct {
+	Url      string `yaml:"url" env:"GIOBBA_BROKER_URL"`
+	Port     string `yaml:"port" env:"GIOBBA_BROKER_PORT"`
+	Username string `yaml:"username" env:"GIOBBA_BROKER_ADMIN_USERNAME"`
+	Password string `yaml:"password" env:"GIOBBA_BROKER_ADMIN_PASSWORD"`
+  DB       string `yaml:"db" env:"GIOBBA_BROKER_ADMIN_PASSWORD"`
+}
+
+type Config struct {
+	Name     string   `yaml:"name"`
+	Version  string   `yaml:"version" env:"APP_VERSION"`
+	Database Database `yaml:"database"`
+  Broker   Broker   `yaml:"broker"`
+}
+
+func LoadConfig() (*Config, error) {
+
+	// Determine environment
+	env := os.Getenv("GIOBBA_ENV")
+	if env != "" {
+		env = "." + env
+	}
+
+	configFileExt := []string{".yml", ".yaml"}
+
+	homePath, ok := os.LookupEnv("HOME")
+	if ok {
+		homePath += "/.giobba"
+	}
+
+	// Add config paths
+	configPaths := []string{
+		"/etc/giobba.d",
+		homePath,
+		"./config",
+	}
+
+	var err error
+	var config *Config
+	for i := range len(configPaths) {
+
+		for _, ext := range configFileExt {
+			path := configPaths[i] + ConfigBaseFileName + env + ext
+
+			_, err = os.Stat(path)
+			if err == nil {
+				config = ConfigFromYaml(path)
+				if config != nil {
+					OverrideEnvs(config)
+					err = nil
+					break
+				} else {
+					err = errors.New("Something went wrong")
+				}
+				break
+			}
+		}
+
+	}
+
+	return config, err
+}
+
+func ConfigFromYaml(filePath string) *Config {
+
+	// Open the YAML file
+	log.Printf("ConfigFromYaml - Loading file: %v", filePath)
+	file, err := os.Open(filePath)
+	defer file.Close()
+
+	if err != nil {
+		return nil
+	}
+
+	// Decode the YAML file into the struct
+	decoder := yaml.NewDecoder(file)
+
+	var config = &Config{}
+	err = decoder.Decode(config)
+	if err != nil {
+		log.Printf("ConfigFromYaml - Error Decoding: %v", err)
+	}
+
+	// Print the loaded data
+	log.Printf("ConfigFromYaml - Config Loaded!")
+
+	return config
+}
+
+func OverrideEnvs(obj interface{}) error {
+	return setFieldFromEnv(reflect.ValueOf(obj))
+}
+
+func setFieldFromEnv(v reflect.Value) error {
+
+	t := v.Type()
+
+	// If it's a pointer, get the underlying element
+	if t.Kind() == reflect.Ptr {
+		v = v.Elem()
+		t = v.Type()
+	}
+
+	// If it's not a struct, we're done
+	if t.Kind() != reflect.Struct {
+		return nil
+	}
+
+	// Iterate through all fields
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fieldValue := v.Field(i)
+
+		// If the field is a struct, recursively process it
+		if field.Type.Kind() == reflect.Struct {
+			if err := setFieldFromEnv(fieldValue); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// Check if field has env tag
+		checkEnv(field, fieldValue)
+	}
+
+	return nil
+}
+
+func checkEnv(field reflect.StructField, fieldValue reflect.Value) {
+	// Get environment variable value
+	envTag := field.Tag.Get("env")
+	if envTag != "" {
+		envValue := os.Getenv(envTag)
+		if envValue != "" {
+			// Set the field value
+			if fieldValue.CanSet() {
+				fieldValue.SetString(envValue)
+			}
+		}
+	}
+}
