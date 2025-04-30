d16fa3379a7bb7bace3f01aadb288d200cd7c79b
joeg-ita <joeg.ita@gmail.com>
2025-04-30 09:57:24 +0200

    Update giobba_start.go

--- src/usecases/giobba_start.go
+++ src/usecases/giobba_start.go
@@ -18,8 +18,15 @@ func NewGiobbaStart(scheduler *services.Scheduler) *GiobbaStart {
 func (s *GiobbaStart) Run() {
 
 	for name, handler := range handlers.Handlers {
-		s.Scheduler.RegisterHandler(name, handler)
+		if checkInterface(handler) {
+			s.Scheduler.RegisterHandler(name, handler)
+		}
 	}
 
 	s.Scheduler.Start()
+}
+
+func checkInterface(value interface{}) bool {
+    _, ok := value.(services.TaskHandlerInt)
+    return ok
 }
